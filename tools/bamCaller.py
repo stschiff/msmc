6eeeae543da794e4da4439de482dd8d151b113ac
Stephan Schiffels <stephan.schiffels@mac.com>
2014-04-14 13:45:10 +0100

    modified calling and phasing pipeline substantially

---
+++ tools/bamCaller.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+
+# Use as: samtools mpileup -q 20 -Q 20 -C 50 -g -r <chr> -f <ref_file.fa> <input.bam> | bcftools view -cgI - | bamCaller.py <depth> <out_mask> > <out_vcf>
+# or with new samtools:
+# samtools-exp-rc mpileup -q 20 -Q 20 -C 50 -g -r <chr> -f <ref_file.fa> <input.bam> | bcftools-exp-rc call -c -S indels | bamCaller.py <depth> <out_mask> [Options...] | bcftools-exp-rc view -O z > <out_vcf.gz>
+
+import sys
+import re
+import gzip
+import utils
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("depth", type=float)
+parser.add_argument("mask")
+parser.add_argument("--minMapQ", type=float, default=20)
+parser.add_argument("--minConsQ", type=float, default=20)
+parser.add_argument("--legend_file", help="Impute2 reference panel legend file, can be gzipped or not")
+args = parser.parse_args()
+
+sites_parser = None
+if args.legend_file is not None:
+    sites_parser = utils.LegendParser(args.legend_file)
+
+minDepth = args.depth / 2.0
+maxDepth = args.depth * 2.0
+
+lastPos = 0
+line_cnt = 0
+chr_ = ""
+for line in sys.stdin:
+    if line[0] == '#':
+        print(line, end="")
+        continue
+    fields = line.strip().split('\t')
+    if chr_ == "":
+        chr_ = fields[0]
+        mask = utils.MaskGenerator(args.mask, chr_)
+    else:
+        assert fields[0] == chr_, "found multiple chromosomes"
+    pos = int(fields[1])
+    refAllele = fields[3]
+    altAllele = fields[4]
+    info = fields[7]
+    genotypes = fields[9]
+    if line_cnt % 10000 == 0:
+        print("parsing position {}".format(pos), file=sys.stderr)
+    line_cnt += 1
+
+    if sites_parser is not None:
+        while not sites_parser.end and sites_parser.pos < pos:
+            sites_parser.tick()
+
+    if re.match("^[ACTGactg]$", refAllele) and re.match("^[ACTGactg\.]$", altAllele):
+        dp_match = re.search("DP=(\d+)", info)
+        mq_match = re.search("MQ=(\d+)", info)
+        fq_match = re.search("FQ=([\d-]+)", info)
+        if not (dp_match and mq_match and fq_match):
+            continue
+        dp = int(dp_match.group(1))
+        mq = int(mq_match.group(1))
+        fq = int(fq_match.group(1))
+        if dp >= minDepth and dp <= maxDepth and mq >= args.minMapQ and abs(fq) >= args.minConsQ:
+            mask.addCalledPosition(pos)
+            if altAllele != "." and re.match("^[01][/|][01]", genotypes):
+                print(line, end="")
+            elif sites_parser is not None and sites_parser.pos == pos:
+                assert refAllele == sites_parser.ref_a
+                fields[4] = sites_parser.alt_a
+                print("\t".join(fields))
+
--- tools/cgCaller.py
+++ tools/cgCaller.py
@@ -1,80 +1,104 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import sys
 import argparse
 import gzip
+import bz2
 import re
 import string
-import mask_generator
+import utils
 
 argparser = argparse.ArgumentParser()
 argparser.add_argument("chr", help="Chromosome in the masterVar file")
 argparser.add_argument("sample_id", help="Sample ID to put into the VCF file")
-argparser.add_argument("out_prefix", help="Prefix for mask- and vcf file")
-argparser.add_argument("input", help="Complete Genomics masterVarBeta file (uncompressed)")
+argparser.add_argument("out_mask", help="Prefix for mask- and vcf file")
+argparser.add_argument("input", help="Complete Genomics masterVarBeta file (uncompressed or compressed with gzip or bzip2)")
 argparser.add_argument("--max_pos", type=int, default=0)
+argparser.add_argument("--legend_file", help="Impute2 reference panel legend file, can be gzipped or not")
 args = argparser.parse_args()
 
-mask_file = args.out_prefix + ".mask.bed.gz"
-mask_generator = mask_generator.MaskGenerator(mask_file, args.chr)
-vcf_file = gzip.open(args.out_prefix + ".vcf.gz", "w")
+mask_generator = utils.MaskGenerator(args.out_mask, args.chr)
+sites_parser = None
+if args.legend_file is not None:
+    sites_parser = utils.LegendParser(args.legend_file)
 
-vcf_file.write("##fileformat=VCFv4.1\n")
-vcf_file.write("#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\t{}\n".format(args.sample_id))
+print("##fileformat=VCFv4.1")
+print('##FORMAT=<ID=GT,Number=1,Type=String,Description="Phased Genotype">')
 
-input_file = open(args.input, "r")
+print("#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\t{}".format(args.sample_id))
+
+input_file = None
+if args.input[-3:] == ".gz":
+    input_file = gzip.open(args.input, "rt")
+elif args.input[-4:] == ".bz2":
+    input_file = bz2.open(args.input, "rt")
+else:
+    input_file = open(args.input, "rt")
+    
 line_count = 0
 chromosome_read = False
 for line in input_file:
-  if line[0] == '#' or line[0] == '>' or line == "\n":
-    continue
-  line_count += 1
-  if line_count % 10000 == 0:
-    sys.stderr.write("processing line {}\n".format(line_count))
+    if line[0] == '#' or line[0] == '>' or line == "\n":
+        continue
+    line_count += 1
+    if line_count % 100000 == 0:
+        sys.stderr.write("processing line {}\n".format(line_count))
+      
+    fields = line.strip().split()
     
-  fields = string.split(string.strip(line))
-
-  chrom = fields[2]
-  begin = int(fields[3])
-  end = int(fields[4])
-  
-  if chrom != args.chr:
-    if chromosome_read:
-      break
-    else:
-      continue
-
-  if args.max_pos > 0 and end > args.max_pos:
-      break
-  chromosome_read = True
-  zygosity = fields[5]
-  var_type = fields[6]
-  
-  if var_type == "ref" and zygosity == "hom":
-    for i in xrange(begin + 1, end + 1):
-      mask_generator.addCalledPosition(i)
-
-  if var_type == "snp":
-    if zygosity in ["hom", "het-ref", "het-alt"]:
-      assert end - begin == 1
-      allele_ref = fields[7]
-      allele_1 = fields[8]
-      allele_2 = fields[9]
-      allele1_qual = fields[14]
-      allele2_qual = fields[15]
-      if allele1_qual == "VQHIGH" and allele2_qual == "VQHIGH":
-        mask_generator.addCalledPosition(begin + 1)
-        allele_indices = []
-        alt_alleles = []
-        if allele_1 != allele_ref:
-          alt_alleles.append(allele_1)
-          allele_indices.append(1)
-        if allele_2 == allele_ref:
-          allele_indices.append(0)
-        elif allele_2 == allele_1:
-          allele_indices.append(1)
+    chrom = fields[2]
+    begin = int(fields[3])
+    end = int(fields[4])
+    
+    if chrom != args.chr:
+        if chromosome_read:
+            break
         else:
-          alt_alleles.append(allele_2)
-          allele_indices.append(2)
-        vcf_file.write("{chrom}\t{pos}\t.\t{ref_a}\t{alt_a}\t.\tPASS\t.\tGT\t{gen1}/{gen2}\n".format(chrom=args.chr, 
+            continue
+  
+    if args.max_pos > 0 and end > args.max_pos:
+        break
+    chromosome_read = True
+    zygosity = fields[5]
+    var_type = fields[6]
+    
+    if sites_parser is not None:
+        while not sites_parser.end and sites_parser.pos < begin + 1:
+            sites_parser.tick()
+    
+    if var_type == "ref" and zygosity == "hom":
+        for i in range(begin + 1, end + 1):
+            mask_generator.addCalledPosition(i)
+        if sites_parser is not None:
+            if sites_parser.pos == begin + 1:
+                print("{chrom}\t{pos}\t.\t{ref_a}\t{alt_a}\t.\tPASS\t.\tGT\t0/0".format(chrom=args.chr, pos=begin+1, 
+                                                                                  ref_a=sites_parser.ref_a,
+                                                                                  alt_a=sites_parser.alt_a))
+  
+    if var_type == "snp":
+        if zygosity in ["hom", "het-ref", "het-alt"]:
+            assert end - begin == 1
+            allele_ref = fields[7]
+            if sites_parser is not None:
+                if sites_parser.pos == begin + 1:
+                    assert allele_ref == sites_parser.ref_a
+            allele_1 = fields[8]
+            allele_2 = fields[9]
+            allele1_qual = fields[14]
+            allele2_qual = fields[15]
+            if allele1_qual == "VQHIGH" and allele2_qual == "VQHIGH":
+                mask_generator.addCalledPosition(begin + 1)
+                allele_indices = []
+                alt_alleles = []
+                if allele_1 != allele_ref:
+                    alt_alleles.append(allele_1)
+                    allele_indices.append(1)
+                if allele_2 == allele_ref:
+                    allele_indices.append(0)
+                elif allele_2 == allele_1:
+                    allele_indices.append(1)
+                else:
+                    alt_alleles.append(allele_2)
+                    allele_indices.append(2)
+                print("{chrom}\t{pos}\t.\t{ref_a}\t{alt_a}\t.\tPASS\t.\tGT\t{gen1}/{gen2}".format(chrom=args.chr, 
                        pos=begin+1, ref_a=allele_ref, alt_a=",".join(alt_alleles), gen1=allele_indices[0], gen2=allele_indices[1]))
